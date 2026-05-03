2c549d7c2ec11391409717ce6e23a73e5ef40e6c
Nishant Kumar <nishant098097@gmail.com>
2026-05-03 21:27:52 +0530

    SQL

---
+++ DMLCommands.sql
@@ -0,0 +1,14 @@
+/* DML Commands :- used to insert,update and delete from the row.
+
+Insert:- Adds new rows to table
+
+	M-1:- Manual Entry Values
+	M-2:- Insert Using Select
+
+*/
+
+
+
+-- M-1:- Manual Entry Values
+
+use My
